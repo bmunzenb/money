158a7eb973b935b9b22d80de7a908a9c2b8b8994
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2019-11-15 14:35:15 -0500

    rename files

--- app/src/main/kotlin/com/munzenberger/money/app/model/Date.kt
+++ app/src/main/kotlin/com/munzenberger/money/app/model/Date.kt
@@ -4,4 +4,4 @@ import java.time.LocalDate
 import java.time.ZoneId
 import java.util.*
 
-fun LocalDate.toDate() = Date.from(atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
+fun LocalDate.toDate(): Date = Date.from(atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
