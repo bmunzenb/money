b43d736ca5d3bad426f899c71cf90322da7198da
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2025-01-20 19:18:41 -0500

    add ktlint

--- buildSrc/src/main/kotlin/money.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/money.kotlin-conventions.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("io.gitlab.arturbosch.detekt")
+    id("org.jlleitschuh.gradle.ktlint")
 }
 
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+}
+
+ktlint {
+    version.set("1.5.0")
 }
 
 group = "com.munzenberger"
