bc7f1d6e0e2d213f55fdc41df921c352e4ac5b57
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2025-01-20 19:01:24 -0500

    replace ktlint with detekt

--- buildSrc/src/main/kotlin/money.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/money.kotlin-conventions.gradle.kts
@@ -1,16 +1,12 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
-    id("org.jlleitschuh.gradle.ktlint")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
-}
-
-ktlint {
-    version = "1.5.0"
 }
 
 group = "com.munzenberger"
